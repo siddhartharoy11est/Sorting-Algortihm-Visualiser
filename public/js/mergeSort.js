82a12a7b04ce54c85fb2e46e5da279e46f765b80
siddhartha <sid.roy.2000@gmail.com>
2021-04-01 00:20:19 +0530

    Add Algorithm details and change array size

--- public/js/mergeSort.js
+++ public/js/mergeSort.js
@@ -1,113 +1,72 @@
-// function merge(left, right) {
-//     let arr = []
-//     // Break out of loop if any one of the array gets empty
-//     while (left.length && right.length) {
-//         // Pick the smaller among the smallest element of left and right sub arrays
-//         if (left[0] < right[0]) {
-//             arr.push(left.shift())
-//         } else {
-//             arr.push(right.shift())
-//         }
-//     }
-//
-//     // Concatenating the leftover elements
-//     // (in case we didn't go through the entire left or right array)
-//     return [ ...arr, ...left, ...right ]
-// }
-//
-// function mergeSort(array) {
-//   const half = array.length / 2
-//
-//   // Base case or terminating case
-//   if(array.length < 2){
-//     return array
-//   }
-//
-//   const left = array.splice(0, half)
-//   return merge(mergeSort(left),mergeSort(array))
-// }
+let itmd = [];
+for (var i = 0; i < arr.length; i++) {
+    itmd.push(0)
+}
+const pri=[];
+function mergeArray(start, end) {
+    let mid = parseInt((start + end) >> 1);
+    let start1 = start, start2 = mid + 1
+    let end1 = mid, end2 = end
 
-////////////////////////////////////////////clement code/////////////////////////////////////////////////////
-function mergeSortHelper(
-  mainArray,
-  startIdx,
-  endIdx,
-  auxiliaryArray,
-  animations,
-) {
-  if (startIdx === endIdx) return;
-  const middleIdx = Math.floor((startIdx + endIdx) / 2);
-  mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
-  mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
-  doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+    // Initial index of merged subarray
+    let index = start
+
+    while (start1 <= end1 && start2 <= end2) {
+        if (arr[start1] <= arr[start2]) {
+            itmd[index] = arr[start1]
+            // pri.push([index, start1, arr[start1], arr[start2]])
+            index = index + 1
+            start1 = start1 + 1;
+        }
+        else if(arr[start1] > arr[start2]) {
+            itmd[index] = arr[start2]
+            // pri.push([index, start2, arr[start2], arr[start1]])
+            index = index + 1
+            start2 = start2 + 1;
+        }
+    }
+
+    // Copy the remaining elements of
+    // arr[], if there are any
+    while (start1 <= end1) {
+        itmd[index] = arr[start1]
+        // pri.push([ start1, index, arr[start1], arr[start1]])
+        index = index + 1
+        start1 = start1 + 1;
+    }
+
+    while (start2 <= end2) {
+        itmd[index] = arr[start2]
+        // pri.push([start2, index, arr[start2], arr[start2]])
+        index = index + 1
+        start2 = start2 + 1;
+    }
+
+    index = start
+    while (index <= end) {
+        arr[index] = itmd[index];
+        pri.push([index, index, arr[index], arr[index]])
+        index++;
+    }
+
 }
 
-function doMerge(
-  mainArray,
-  startIdx,
-  middleIdx,
-  endIdx,
-  auxiliaryArray,
-  animations,
-) {
-  let k = startIdx;
-  let i = startIdx;
-  let j = middleIdx + 1;
-  while (i <= middleIdx && j <= endIdx) {
-    // These are the values that we're comparing; we push them once
-    // to change their color.
-    animations.push([i, j]);
-    // These are the values that we're comparing; we push them a second
-    // time to revert their color.
-    animations.push([i, j]);
-    if (auxiliaryArray[i] <= auxiliaryArray[j]) {
-      // We overwrite the value at index k in the original array with the
-      // value at index i in the auxiliary array.
-      animations.push([k, auxiliaryArray[i]]);
-      mainArray[k++] = auxiliaryArray[i++];
-    } else {
-      // We overwrite the value at index k in the original array with the
-      // value at index j in the auxiliary array.
-      animations.push([k, auxiliaryArray[j]]);
-      mainArray[k++] = auxiliaryArray[j++];
+const mergeSort = (start, end) => {
+    if (start < end) {
+        let mid = parseInt((start + end) >> 1)
+         mergeSort(start, mid)
+         mergeSort(mid + 1, end)
+         mergeArray(start, end)
+         // drawBars(start, end)
+
+        // Waiting time is 800ms
+         // timeout(800)
     }
-  }
-  while (i <= middleIdx) {
-    // These are the values that we're comparing; we push them once
-    // to change their color.
-    animations.push([i, i]);
-    // These are the values that we're comparing; we push them a second
-    // time to revert their color.
-    animations.push([i, i]);
-    // We overwrite the value at index k in the original array with the
-    // value at index i in the auxiliary array.
-    animations.push([k, auxiliaryArray[i]]);
-    mainArray[k++] = auxiliaryArray[i++];
-  }
-  while (j <= endIdx) {
-    // These are the values that we're comparing; we push them once
-    // to change their color.
-    animations.push([j, j]);
-    // These are the values that we're comparing; we push them a second
-    // time to revert their color.
-    animations.push([j, j]);
-    // We overwrite the value at index k in the original array with the
-    // value at index j in the auxiliary array.
-    animations.push([k, auxiliaryArray[j]]);
-    mainArray[k++] = auxiliaryArray[j++];
-  }
 }
 
 function merges(){
-  // console.log("In merge fn");
-  // let animat = [];
-  // mergeSort(arr, animat);
-  //   console.log(animat);
-  // animate(animat);
-
-  const animations = [];
-  // if (array.length <= 1) return array;
-  const auxiliaryArray = arr.slice();
-  mergeSortHelper(arr, 0, arr.length - 1, auxiliaryArray, animations);
-  quickanimate(animations);
+  console.log(arr);
+  mergeSort( 0, arr.length-1);
+  console.log(arr);
+  quickanimate(pri);
 }
